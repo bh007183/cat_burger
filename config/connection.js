fc8ac6e847a2a1dba9c5cfcb20b497af25d0e477
Ben Hopkins <bjhops17@gmail.com>
2021-01-25 16:47:38 -0800

    tetst

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 let connection;
 if(process.env.JAWSDB_URL){
-connection == mysql.createConnection(process.env.JAWSDB_URL)
+connection = mysql.createConnection(process.env.JAWSDB_URL)
 }else{
   connection = mysql.createConnection({
 
